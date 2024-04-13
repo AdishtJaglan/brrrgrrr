a256b219dc4a4edde07da3680ebc35aa4ca0c8cd
AdishtJaglan <adisht7524@gmail.com>
2024-04-13 14:37:32 +0530

    add modal to create burger

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("Connected!!");
+const show_button = document.querySelector(".btn-show");
+const close_button = document.querySelector(".btn-close");
+const dialog = document.querySelector("dialog");
+const burgers = [];
+
+function Burger(ingredient, patty, sauce) {
+    this.ingredient = ingredient;
+    this.patty = patty;
+    this.sauce = sauce;
+}
+
+function add_burger(ingredient, patty, sauce) {
+    const newBurger = new Burger(ingredient, patty, sauce);
+
+    burgers.push(newBurger);
+
+    return burgers;
+}
+
+//opening the modal
+show_button.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+//closing the modal
+close_button.addEventListener("click", () => {
+    dialog.close();
+});
