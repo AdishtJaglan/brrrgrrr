75e3a80c228b5950e5c5e414d5d2cbcfd372e61e
AdishtJaglan <adisht7524@gmail.com>
2024-04-13 16:24:44 +0530

    create two separate arrays

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ const option_form = document.querySelector(".option-form");
 const dialog_custom = document.querySelector(".dialog-custom");
 const dialog_option = document.querySelector(".dialog-option");
 
-const burgers = [];
+const custom_burgers = [];
+const option_burgers = [];
 
 function Burger(ingredient, patty, sauce) {
     this.ingredient = ingredient;
@@ -17,12 +18,20 @@ function Burger(ingredient, patty, sauce) {
     this.sauce = sauce;
 }
 
-function add_burger(ingredient, patty, sauce) {
+function add_burger(ingredient, patty, sauce, type) {
     const newBurger = new Burger(ingredient, patty, sauce);
 
-    burgers.push(newBurger);
+    if (type) {
+        custom_burgers.push(newBurger);
 
-    return burgers;
+        return custom_burgers;
+    }
+
+    if (!type) {
+        option_burgers.push(newBurger);
+
+        return option_burgers;
+    }
 }
 
 //opening the custom modal
@@ -53,9 +62,9 @@ custom_form.addEventListener("submit", (e) => {
     const patty_value = patty.value;
     const sauce_value = sauce.value;
 
-    add_burger(ingredient_value, patty_value, sauce_value);
+    add_burger(ingredient_value, patty_value, sauce_value, 1);
 
-    console.log("burger: ", burgers);
+    console.log("custom burger: ", custom_burgers);
 
     dialog_custom.close();
 });
@@ -68,9 +77,9 @@ option_form.addEventListener("submit", (e) => {
     const patty_value = patty.value;
     const sauce_value = sauce.value;
 
-    add_burger(ingredient_value, patty_value, sauce_value);
+    add_burger(ingredient_value, patty_value, sauce_value, 0);
 
-    console.log("burger: ", burgers);
+    console.log("option burger: ", option_burgers);
 
-    dialog_custom.close();
+    dialog_option.close();
 });
